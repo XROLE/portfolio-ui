d63ce561a938e63de14dce0af7c1adeb20c23a05
XROLE <xrolediamond@gmail.com>
2018-09-01 07:55:40 -0700

    adjusted the marging of the navbar items

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,10 +10,7 @@ class Navbar extends Component{
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="github.com/xrole">Home <span class="sr-only">(current)</span></a>
-                        </li>
+                    <ul className="navbar-nav mr-auto">                        
                         <li className="nav-item">
                             <a className="nav-link" href="github.com/xrole">About</a>
                         </li>        
