260fc07a504f2c2d5d806fb0df3749a70a994919
XROLE <xrolediamond@gmail.com>
2018-09-01 23:55:00 -0700

    cliped the landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -4,16 +4,18 @@ import './App.css';
 class LandingPage extends Component {
     render(){
         return (
-            <div className = 'container landingPage'>
-                <div className='row' >
-                    <div className ="col-12 col-md-6 col-lg-6">
-                        <h1 className="meetXrole">Meet Xrole Diamond</h1>
-                        <p>A credible top nouch software developer</p>
-                        <a className ="btn btn-success">Hire Me </a>
-                    </div>
-                    <div className ="col-12 col-md-6 col-lg-6">
-                        <p className="landingPage-image"><img className ="img-fluid" src = "./images/xrole.png" alt = "landing page image"/></p>
-                    </div>
+            <div className = 'container-fluid landingPage'>
+                <div className ="container">
+                    <div className='row' >
+                        <div className ="col-12 col-md-6 col-lg-6">
+                            <h1 className="meetXrole">Meet Xrole Diamond</h1>
+                            <p>A credible top nouch software developer</p>
+                            <a className ="btn btn-success">Hire Me </a>
+                        </div>
+                        <div className ="col-12 col-md-6 col-lg-6">
+                            <p className="landingPage-image"><img className ="img-fluid" src = "./images/xrole.png" alt = "landing page image"/></p>
+                        </div>
+                    </div>                
                 </div>
             </div>
         )
