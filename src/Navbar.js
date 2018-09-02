9ff678c08f1ac8b63939781479d527602e69dc9f
XROLE <xrolediamond@gmail.com>
2018-09-01 23:21:03 -0700

    handled the visibility of navbar toggler background

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import './App.css';
 class Navbar extends Component{
     render(){
         return (
-            <nav className="navbar navbar-expand-lg " >
+            <nav className="navbar navbar-expand-lg" >
                 <a className="navbar-brand" href="github.com/xrole">Meet Xrole</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
                     <span className="navbar-toggler-icon"></span>
