d33532fc5d0a92eb55453b476ffa8c7dd0d3baca
XROLE <xrolediamond@gmail.com>
2018-09-03 12:59:51 -0700

    aligned the parallax text

--- src/Parallax.js
+++ src/Parallax.js
@@ -4,11 +4,12 @@ import './App.css';
 class Parallax extends Component {
     render(){
         return(
-            <div className =" contaner-fluid parallax">
+            <div className =" contaner-fluid  parallax">
                 <div className = "container">
                     <div className = "row">
                         <div className = "col">
-                            <p className = "parralaxText"> GET YOUR PROJECT DONE WITHIN 72 HOURS</p>            
+                            <p className = "parralaxText"> GET YOUR PROJECT DONE WITHIN 72 HOURS</p>  
+                            {/* please fix the parallax isuue later           */}
                         </div>
                     </div>
                 </div>
