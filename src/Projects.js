114e263f153f5ae139ddf2c9fa89c09f4039327a
XROLE <xrolediamond@gmail.com>
2018-09-04 12:18:02 -0700

    finished adding links of Projects done

--- src/Projects.js
+++ src/Projects.js
@@ -8,24 +8,36 @@ class Projects extends Component{
                 <div className ="container">
                     <div className = "row projectRow">
                         <div class="card projectCard">
-                            <img class="card-img-top" src='./images/currencyConverter.png' alt="Card image cap" />
+                            <a href = "https://xrole.github.io/CurrencyConverter/" target = "_blank">
+                                <img class="card-img-top" src='./images/currencyConverter.png' alt="Card image cap" />
+                            </a>
                         </div>
                         <div class="card projectCard">
-                            <img class="card-img-top" src='./images/serviceWorker.png' alt="Card image cap" />
+                            <a href = "https://xrole.herokuapp.com/" target = "_blank">
+                                <img class="card-img-top" src='./images/serviceWorker.png' alt="Card image cap" />
+                            </a>
                         </div>
                         <div class="card projectCard">
-                            <img class="card-img-top" src='./images/exchangeRate.png' alt="Card image cap" />
+                            <a href = "https://xrole.github.io/exchangeRate/" target = "_blank">
+                                <img class="card-img-top" src='./images/exchangeRate.png' alt="Card image cap" />
+                            </a>
                         </div>
                     </div>
                     <div className = "row projectRow">
                         <div class="card projectCard">
-                            <img class="card-img-top" src='./images/ceasarCipher.png' alt="Card image cap" />
+                            <a href = "https://xrole.github.io/Ceasar-Cipher/" target = "_blank">
+                                <img class="card-img-top" src='./images/ceasarCipher.png' alt="Card image cap" />
+                            </a>
                         </div>
                         <div class="card projectCard">
-                            <img class="card-img-top" src='./images/myDiary.png' alt="Card image cap" />
+                            <a href = "https://xrole.github.io/My-Diary/UI/" target = "_blank">
+                                <img class="card-img-top" src='./images/myDiary.png' alt="Card image cap" />
+                            </a>
                         </div>
                         <div class="card projectCard">
-                            <img class="card-img-top" src='./images/payFriend.png' alt="Card image cap" />
+                            <a href = "https://xrole.github.io/Frontend-test/" target = "_blank">
+                                <img class="card-img-top" src='./images/payFriend.png' alt="Card image cap" />
+                            </a>
                         </div>
                     </div>
                 </div>
