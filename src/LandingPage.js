88ada2cb83d689994d57d0fc495ca28975c8ff3e
XROLE <xrolediamond@gmail.com>
2018-09-04 12:01:24 -0700

    fixed landing page link

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -10,7 +10,7 @@ class LandingPage extends Component {
                         <div className ="col-12 col-md-6 col-lg-6">
                             <h1 className="meetXrole">Meet Xrole Diamond</h1>
                             <p>A credible top nouch software developer</p>
-                            <a className ="btn btn-success">Hire Me </a>
+                            <a className ="btn btn-success" href = "#Hireme">Hire Me </a>
                         </div>
                         <div className ="col-12 col-md-6 col-lg-6">
                             <p className="landingPage-image"><img className ="img-fluid landing-image" src = "./images/xrole.png" alt = "landing page image"/></p>
